5188fe40b6cb4df5f5a7d5b347621fce1f5d2b63
Lea <112546991+t2hale00@users.noreply.github.com>
2023-04-25 12:38:07 +0300

    erased feedbackbd

--- server/feedbackdb.sql
+++
@@ -1,9 +0,0 @@
-create database feedback;
-
-create table feedback (
-    id int primary key auto_increment,
-    decription varchar(255) not null
-);
-
-insert into feedback  (description) values ('My test feedback');
-insert into feedback (description) values ('My another test feedback');
