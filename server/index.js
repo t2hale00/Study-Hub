a77c9b5c095bf2ded2254d483fb7da243322eb5b
Ayomide Mary Afolabi <92940837+kepsTech@users.noreply.github.com>
2023-05-01 22:30:01 +0300

    added new changes (#1)

--- server/index.js
+++ server/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -15,11 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const pg_1 = require("pg");
+const uuid_1 = require("uuid");
 //this is to parse request body for POST requests
 const app = (0, express_1.default)();
-const { createToken } = require('./auth');
-const path = require("path");
-const knex = require('knex');
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -28,100 +17,125 @@ app.use(bodyParser.urlencoded({
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
-const port = 3001;
-/*let initialPath = path.join(__dirname, "public");
-
-app.use(express.static(initialPath));
-
-app.get('/', (req, res)=> {
-    res.sendFile(path.join(initialPath, "index.html"));
-})
-
-app.get("/login", (req,res) => {
-    res.sendFile (path.join(initialPath, "login.html"));
-});
-
-app.get("/register", (req,res) => {
-    res.sendFile (path.join(initialPath, "signup.html"));
-});
-
-
-
-app.listen (3000,() => {
-    console.log ("Example app listening on port 3000!");
-});
-*/
-app.post("/login", (req, res) => {
-    // Read username and password from request body
-    const { username, password } = req.body;
-    // Console log that somebody is trying to log in
-    console.log(`Trying to log in with username: ${username} and password: ${password}`);
-    // Check if username and password are valid
-    if (username === "john@gmail.com" && password === "password") {
-        // Create a JWT token
-        const token = createToken(username);
-        // Login successful
-        return res.json({ message: "Login successful!", token: token });
-    }
-    // Login failed
-    return res.status(401).send("Login failed!");
-});
-app.get("/profile", (req, res) => {
-    res.send("Hello profile!");
-});
-app.delete("/delete/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+const port = 3002;
+app.post('/register', (req, res) => {
     const pool = openDb();
-    const id = parseInt(req.params.id);
-    pool.query('delete from feedback where id = $1', [id], (error, result) => {
+    pool.query('insert into users (name, email, pwdhash) values ($1, $2, $3) returning *', [req.body.name, req.body.email, req.body.pwdhash], (error, result) => {
         if (error) {
             res.status(500).json({ error: error.message });
         }
-        res.status(200).json({ id: id });
+        res.status(201).json({ name: result.rows[0].name });
     });
-}));
-app.post('/new', (req, res) => {
+});
+app.post('/login', (req, res) => {
     const pool = openDb();
-    pool.query('insert into feedback (description) values ($1) returning *', [req.body.description], (error, result) => {
+    pool.query('select * from users where email = $1 and pwdhash = $2', [req.body.email, req.body.pwdhash], (error, result) => {
         if (error) {
             res.status(500).json({ error: error.message });
         }
-        res.status(200).json({ id: result.rows[0].id });
+        if (result.rows.length === 0) {
+            res.status(403).json({ result: 'forbidden' });
+        }
+    });
+    let auth = (0, uuid_1.v4)();
+    pool.query('UPDATE users SET auth_id = $1 WHERE email = $2 returning *', [auth, req.body.email], (error, result) => {
+        if (error) {
+            res.status(500).json({ error: error.message });
+        }
+        res.status(200).json({ email: result.rows[0].email, auth: result.rows[0].auth_id });
     });
 });
-app.get('/', (req, res) => {
-    //res.status(200).json({result: 'success'})
+app.post('/questions', (req, res) => {
     const pool = openDb();
-    pool.query('select * from feedback', (error, result) => {
+    let user_name = "";
+    pool.query('select * from users where auth_id = $1 and user_id = $2', [req.query.auth_id, req.body.user_id], (error, result) => {
+        if (error) {
+            res.status(500).json({ error: error.message });
+        }
+        if (result.rows.length === 0) {
+            res.status(403).json({ result: 'forbidden' });
+        }
+        user_name = result.rows[0].name;
+    });
+    pool.query('insert into question (user_id, questions) values ($1, $2) returning *', [req.body.user_id, req.body.questions], (error, result) => {
+        if (error) {
+            res.status(500).json({ error: error.message });
+        }
+        res.status(201).json({ name: user_name, question: result.rows[0].questions });
+    });
+});
+app.get('/questions', (req, res) => {
+    const pool = openDb();
+    pool.query('select u.name, q.question_id, q.questions, q.created_at from question q join users u on q.user_id = u.user_id', (error, result) => {
         if (error) {
             res.status(500).json({ error: error.message });
         }
         res.status(200).json(result.rows);
     });
 });
+app.post('/addcomment', (req, res) => {
+    const pool = openDb();
+    let user_id;
+    pool.query('select * from users where auth_id = $1', [req.query.auth_id], (error, result) => {
+        if (error) {
+            res.status(500).json({ error: error.message });
+        }
+        if (result.rows.length === 0) {
+            res.status(403).json({ result: 'forbidden' });
+        }
+        user_id = result.rows[0].user_id;
+    });
+    pool.query('insert into comments (question_id, comment, user_id) values ($1, $2, $3) returning *', [req.body.question_id, req.body.comment, req.body.user_id], (error, result) => {
+        if (error) {
+            res.status(500).json({ error: error.message });
+        }
+        res.status(201).json(result.rows);
+    });
+});
+app.post('/addcomment', (req, res) => {
+    const pool = openDb();
+    let user_id;
+    pool.query('select * from users where auth_id = $1', [req.query.auth_id], (error, result) => {
+        if (error) {
+            res.status(500).json({ error: error.message });
+        }
+        if (result.rows.length === 0) {
+            res.status(403).json({ result: 'forbidden' });
+        }
+        user_id = result.rows[0].user_id;
+    });
+    pool.query('insert into comments (question_id, comment, user_id) values ($1, $2, $3) returning *', [req.body.question_id, req.body.comment, req.body.user_id], (error, result) => {
+        if (error) {
+            res.status(500).json({ error: error.message });
+        }
+        res.status(201).json(result.rows);
+    });
+});
+app.delete('/question/:query_id', (req, res) => {
+    const pool = openDb();
+    pool.query('select * from users u join question q on u.user_id = q.user_id where u.auth_id = $1 and q.question_id = $2', [req.query.auth_id, req.params.query_id], (error, result) => {
+        if (error) {
+            res.status(500).json({ error: error.message });
+        }
+        if (result.rows.length === 0) {
+            res.status(403).json({ result: 'forbidden' });
+        }
+    });
+    pool.query('delete from question where question_id = $1', [req.params.query_id], (error, result) => {
+        if (error) {
+            res.status(500).json({ error: error.message });
+        }
+        res.status(200).json({ result: "success" });
+    });
+});
 const openDb = () => {
     const pool = new pg_1.Pool({
         user: 'postgres',
         host: 'localhost',
-        database: 'feedback',
-        password: '1805',
+        database: 'studyhub',
+        password: '1234',
         port: 5432,
-        /*user: 'root',
-        host: 'dpg-ch0fft33cv2c5b40uldg-a.oregon-postgres.render.com',
-        database: 'feedback_3srd',
-        password: '6ZpVgBgPxITJ4Ik8AtjSiXqulEHAPAkK',
-        port: 5432,
-        ssl: true
-*/
     });
     return pool;
 };
-const db = knex({
-    client: 'pg',
-    connection: {
-        host: 'localhost',
-        user: 'postgres',
-        password: '1805',
-        database: 'studyhub'
-    }
-});
 app.listen(port);
--- server/index.ts
+++ server/index.ts
@@ -2,11 +2,12 @@ import express, { Express, Request, Response } from 'express'
 import cors from 'cors'
 import { Pool } from 'pg'
 import { QueryResult } from 'pg'
+import { error } from 'console';
+import {v4 as uuidv4} from 'uuid';
+
 //this is to parse request body for POST requests
 const app: Express = express();
-const { createToken } = require('./auth');
-const path = require("path");
-const knex = require ('knex');
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(
@@ -18,124 +19,180 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-const port = 3001
+const port = 3002
 
-/*let initialPath = path.join(__dirname, "public");
-
-app.use(express.static(initialPath));
-
-app.get('/', (req, res)=> {
-    res.sendFile(path.join(initialPath, "index.html"));
-})
-
-app.get("/login", (req,res) => {
-    res.sendFile (path.join(initialPath, "login.html"));
-});
-
-app.get("/register", (req,res) => {
-    res.sendFile (path.join(initialPath, "signup.html"));
-});
-
-
-
-app.listen (3000,() => {
-    console.log ("Example app listening on port 3000!");
-});
-*/
-
-app.post("/login", (req, res) => {
-    // Read username and password from request body
-    const { username, password } = req.body;
-
-    // Console log that somebody is trying to log in
-    console.log(`Trying to log in with username: ${username} and password: ${password}`);
-
-    // Check if username and password are valid
-    if (username === "john@gmail.com" && password === "password") {
-        // Create a JWT token
-        const token = createToken(username);
-        // Login successful
-        return res.json({ message: "Login successful!", token: token });
-    }
-    // Login failed
-    return res.status(401).send("Login failed!");
-});
-
-
-
-
-app.get("/profile", (req,res) => {
-    res.send ("Hello profile!");
-});
-
-
-app.delete("/delete/:id",async(req: Request,res: Response) => {
+app.post('/register', (req:Request,res:Response) => {
     const pool = openDb()
-
-    const id = parseInt(req.params.id)
-
-    pool.query('delete from feedback where id = $1',
-    [id],
-    (error: Error,result: QueryResult) => {
-        if(error) {
-            res.status(500).json({error: error.message})
-        }
-
-        res.status(200).json({id: id})
-    })
-})
-
-
-app.post('/new',(req: Request, res: Response) => {
-    const pool = openDb()
-
-    pool.query('insert into feedback (description) values ($1) returning *',
-    [req.body.description],
-    (error: Error,result: QueryResult) => {
+    pool.query('insert into users (name, email, pwdhash) values ($1, $2, $3) returning *', 
+    [req.body.name, req.body.email, req.body.pwdhash], 
+    (error: Error, result: QueryResult) => {
         if (error) {
-            res.status(500).json({error: error.message})
+            res.status(500).json({error:error.message})
         }
-        res.status(200).json({id: result.rows[0].id})
+
+        res.status(201).json({name: result.rows[0].name})  
     })
 })
 
-app.get('/', (req: Request, res: Response) => {
-    //res.status(200).json({result: 'success'})
+app.post('/login', (req:Request,res:Response) => {
     const pool = openDb()
 
-    pool.query('select * from feedback', (error, result) => {
-      if (error) {
-        res.status(500).json({error: error.message})
+    pool.query('select * from users where email = $1 and pwdhash = $2',
+    [req.body.email, req.body.pwdhash], 
+    (error: Error, result: QueryResult) => {
+        if (error) {
+            res.status(500).json({error:error.message})
         }
-        res.status(200).json(result.rows)
+        if (result.rows.length === 0) {
+            res.status(403).json({result: 'forbidden'}) 
+        }
     })
+
+    let auth = uuidv4();
+    pool.query('UPDATE users SET auth_id = $1 WHERE email = $2 returning *', 
+    [auth, req.body.email], 
+    (error: Error, result: QueryResult) => {
+        if (error) {
+            res.status(500).json({error:error.message})
+        }
+
+       res.status(200).json({email: result.rows[0].email, auth: result.rows[0].auth_id})  
+    })
+
+})
+
+app.post('/questions', (req:Request,res:Response) => {
+    const pool = openDb()
+    let user_name = ""
+   
+   pool.query('select * from users where auth_id = $1 and user_id = $2',
+   [req.query.auth_id, req.body.user_id], 
+   (error: Error, result: QueryResult) => {
+       if (error) {
+           res.status(500).json({error:error.message})
+       }
+       if (result.rows.length === 0) {
+           res.status(403).json({result: 'forbidden'}) 
+       }
+
+       user_name = result.rows[0].name
+   })
+   
+   
+   pool.query('insert into question (user_id, questions) values ($1, $2) returning *', 
+   [req.body.user_id, req.body.questions], 
+   (error: Error, result: QueryResult) => {
+    if (error) {
+        res.status(500).json({error:error.message})
+    }
+
+    res.status(201).json({name: user_name, question: result.rows[0].questions})  
+})
+})
+
+app.get('/questions', (req:Request,res:Response) => {
+    const pool = openDb()
+   
+   pool.query('select u.name, q.question_id, q.questions, q.created_at from question q join users u on q.user_id = u.user_id',
+   (error: Error, result: QueryResult) => {
+       if (error) {
+           res.status(500).json({error:error.message})
+       }
+        res.status(200).json(result.rows) 
+   })
+
+   
+})
+
+app.post('/addcomment', (req:Request,res:Response) => {
+    const pool = openDb()
+    let user_id
+   
+   pool.query('select * from users where auth_id = $1',
+   [req.query.auth_id], 
+   (error: Error, result: QueryResult) => {
+       if (error) {
+           res.status(500).json({error:error.message})
+       }
+       if (result.rows.length === 0) {
+           res.status(403).json({result: 'forbidden'}) 
+       }
+
+       user_id = result.rows[0].user_id
+   })
+
+   pool.query('insert into comments (question_id, comment, user_id) values ($1, $2, $3) returning *', 
+   [req.body.question_id, req.body.comment, req.body.user_id], 
+   (error: Error, result: QueryResult) => {
+        if (error) {
+            res.status(500).json({error:error.message})
+        }
+
+    res.status(201).json(result.rows) 
+    })
+})
+
+app.post('/addcomment', (req:Request,res:Response) => {
+    const pool = openDb()
+    let user_id
+   
+   pool.query('select * from users where auth_id = $1',
+   [req.query.auth_id], 
+   (error: Error, result: QueryResult) => {
+       if (error) {
+           res.status(500).json({error:error.message})
+       }
+       if (result.rows.length === 0) {
+           res.status(403).json({result: 'forbidden'}) 
+       }
+
+       user_id = result.rows[0].user_id
+   })
+
+   pool.query('insert into comments (question_id, comment, user_id) values ($1, $2, $3) returning *', 
+   [req.body.question_id, req.body.comment, req.body.user_id], 
+   (error: Error, result: QueryResult) => {
+        if (error) {
+            res.status(500).json({error:error.message})
+        }
+
+    res.status(201).json(result.rows) 
+    })
+})
+
+app.delete('/question/:query_id', (req:Request,res:Response) => {
+    const pool = openDb()
+   
+    pool.query('select * from users u join question q on u.user_id = q.user_id where u.auth_id = $1 and q.question_id = $2',
+   [req.query.auth_id, req.params.query_id], 
+   (error: Error, result: QueryResult) => {
+       if (error) {
+           res.status(500).json({error:error.message})
+       }
+       if (result.rows.length === 0) {
+           res.status(403).json({error: 'forbidden'}) 
+       }
+   })
+
+   pool.query('delete from question where question_id = $1', 
+   [req.params.query_id], 
+   (error: Error) => {
+        if (error) {
+            res.status(500).json({error:error.message})
+        }
+    }) 
+    res.status(200).json({status: "success"}) 
 })
 
 const openDb = (): Pool => {
     const pool: Pool = new Pool ({  
         user: 'postgres',
         host: 'localhost',
-        database: 'feedback',
-        password: '1805',
+        database: 'studyhub',
+        password: '1234',
         port: 5432,
-        /*user: 'root',
-        host: 'dpg-ch0fft33cv2c5b40uldg-a.oregon-postgres.render.com',
-        database: 'feedback_3srd',
-        password: '6ZpVgBgPxITJ4Ik8AtjSiXqulEHAPAkK',
-        port: 5432,
-        ssl: true
-*/
     })  
     return pool
 }
-const db = knex({
-    client: 'pg',
-    connection: {
-        host: 'localhost',
-        user: 'postgres',
-        password: '1805',
-        database: 'studyhub'
-    }
-})
 
 app.listen(port)
